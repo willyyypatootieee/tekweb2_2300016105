2485d6f838c34a738525685fa6895d5551a90fed
23illy <71692311+willyyypatootieee@users.noreply.github.com>
2024-10-06 23:32:32 +0700

    pertama anjay

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client'; // Change the import source
+import { createRoot } from "react-dom/client";
 import App from './App';
 import './index.css';
 import { insertCoin } from 'playroomkit';
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/tekweb1-2024-2300016105/',  // The repository name
   plugins: [react()],
 })
